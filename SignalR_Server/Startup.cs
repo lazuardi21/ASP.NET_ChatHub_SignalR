893e0dc67254ce13c81ac0628dda771ff0a3341f
lazuardi_fenjano <lazuardifenzan@gmail.com>
2022-01-12 16:53:54 +0700

    Chathub with SignalR

---
+++ SignalR_Server/Startup.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.Owin;
+using Owin;
+using System;
+using System.Threading.Tasks;
+
+[assembly: OwinStartup(typeof(SignalR_Server.Startup))]
+
+namespace SignalR_Server
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            // Any Connection or hub wire and configuration go here
+            app.MapSignalR();
+        }
+    }
+}
