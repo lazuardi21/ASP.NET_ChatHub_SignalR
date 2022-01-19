e5429a1e6abf66270d341eb6153ac06d3e827b9e
lazuardi_fenjano <lazuardifenzan@gmail.com>
2022-01-19 09:11:01 +0700

    Add Send to Specific User

--- SignalR_Server/ChatHub.cs
+++ SignalR_Server/ChatHub.cs
@@ -1,16 +1,31 @@
 ﻿using Microsoft.AspNet.SignalR;
 using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
+using System.Threading.Tasks;
 using System.Web;
 
 namespace SignalR_Server
 {
     public class ChatHub : Hub
     {
-        public void Send(string name, string message)
+        public void Disconnect()
         {
-            Clients.All.broadcastMessage(name, message);
+            Debug.WriteLine(Context.ConnectionId + " disconnected");
+        }
+        public async Task Send(string name, string message)
+        {
+            await Clients.All.broadcastMessage(name, message);
+        }
+
+        public async Task SendMessage(string message, int color, string username)
+        {
+            await Clients.All.UpdateChatMessage(message, color, username);
+        }
+        public async Task SendToUser(string user, string receiverConnectionId, string message)
+        {
+            await Clients.Client(receiverConnectionId).SendAsync(user, message);
         }
     }
 }
